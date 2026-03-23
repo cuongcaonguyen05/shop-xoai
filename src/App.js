221b3da30bdb9b78b6ef7c3632e8da3dfcbd5d81
cuongcaonguyen05 <cuongcaonguyen05@gmail.com>
2026-03-23 23:35:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import HomePage from './UI/HomePage';
 function App() {
   return (
     <Routes>
-      <Route path="/"            element={<HomePage />} />
-      <Route path="/gioi-thieu"  element={<div>Trang Giới thiệu</div>} />
-      <Route path="/san-pham"    element={<div>Trang Sản phẩm</div>} />
-      <Route path="/tin-tuc"     element={<div>Trang Tin tức</div>} />
-      <Route path="/lien-he"     element={<div>Trang Liên hệ</div>} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
